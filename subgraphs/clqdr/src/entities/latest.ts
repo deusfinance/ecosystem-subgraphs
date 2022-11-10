327edb43cb63a59f7bcbd2fcc68ac70b92a7a719
coinlordd <coinlord.developer@gmail.com>
2022-11-10 09:53:35 +0100

    Update latest.ts

--- subgraphs/clqdr/src/entities/latest.ts
+++ subgraphs/clqdr/src/entities/latest.ts
@@ -3,7 +3,7 @@ import {Latest, Snapshot} from '../../generated/schema'
 const LATEST_ID = 'latest'
 
 export function updateLatest(snapshot: Snapshot): void {
-  const latest = getLatest()
+  let latest = getLatest()
   latest.totalReserve = snapshot.totalReserve
   latest.totalSupply = snapshot.totalSupply
   latest.priceShare = snapshot.priceShare
