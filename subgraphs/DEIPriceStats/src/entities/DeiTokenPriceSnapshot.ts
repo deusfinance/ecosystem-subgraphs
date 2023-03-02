2d8d7945149fc5c46cec47271772c6b19974e5eb
Shadow <87962799+shadowcrypto1@users.noreply.github.com>
2023-03-02 20:45:11 +0530

    add hourly and daily snapshots

--- subgraphs/DEIPriceStats/src/entities/DeiTokenPriceSnapshot.ts
+++ subgraphs/DEIPriceStats/src/entities/DeiTokenPriceSnapshot.ts
@@ -1,6 +1,6 @@
-import {Address, BigDecimal, log} from '@graphprotocol/graph-ts'
+import {Address, BigDecimal, BigInt, log} from '@graphprotocol/graph-ts'
 import {DEI_TOKEN_ADDRESS, USDC_TOKEN_ADDRESS} from '../../constants'
-import {DeiTokenPriceSnapshot} from '../../generated/schema'
+import {DailyDeiTokenPriceSnapshot, DeiTokenPriceSnapshot, HourlyDeiTokenPriceSnapshot} from '../../generated/schema'
 import {Swap} from '../../generated/Vault/Vault'
 import {scaleDown} from '../helpers'
 
@@ -44,4 +44,62 @@ export function createDeiTokenPriceSnapshot(event: Swap): DeiTokenPriceSnapshot 
   snapshot.save()
 
   return snapshot
+}
+
+export function updateHourlyDEITokenPriceSnapshot(snapshot: DeiTokenPriceSnapshot): void {
+  const hourlySnapshot = getHourlyDEITokenPriceSnapshot(snapshot.timestamp)
+
+  hourlySnapshot.poolId = snapshot.poolId
+  hourlySnapshot.txHash = snapshot.txHash
+  hourlySnapshot.block = snapshot.block
+  hourlySnapshot.timestamp = snapshot.timestamp
+  hourlySnapshot.deiPrice = hourlySnapshot.deiPrice
+    .times(BigDecimal.fromString(hourlySnapshot.snapshots.length.toString()))
+    .plus(snapshot.deiPrice)
+    .div(BigDecimal.fromString((hourlySnapshot.snapshots.length + 1).toString()))
+
+  const snapshots = hourlySnapshot.snapshots
+  snapshots.push(snapshot.id)
+  hourlySnapshot.snapshots = snapshots
+
+  hourlySnapshot.save()
+}
+
+function getHourlyDEITokenPriceSnapshot(timestamp: BigInt): HourlyDeiTokenPriceSnapshot {
+  const hourlyId = (timestamp.toI32() / (60 * 60)).toString()
+  let hourlySnapshot = HourlyDeiTokenPriceSnapshot.load(hourlyId)
+  if (!hourlySnapshot) {
+    hourlySnapshot = new HourlyDeiTokenPriceSnapshot(hourlyId)
+    hourlySnapshot.timestamp = timestamp
+  }
+  return hourlySnapshot
+}
+
+export function updateDailyDEITokenPriceSnapshot(snapshot: DeiTokenPriceSnapshot): void {
+  const dailySnapshot = getDailyDEITokenPriceSnapshot(snapshot.timestamp)
+
+  dailySnapshot.poolId = snapshot.poolId
+  dailySnapshot.txHash = snapshot.txHash
+  dailySnapshot.block = snapshot.block
+  dailySnapshot.timestamp = snapshot.timestamp
+  dailySnapshot.deiPrice = dailySnapshot.deiPrice
+    .times(BigDecimal.fromString(dailySnapshot.snapshots.length.toString()))
+    .plus(snapshot.deiPrice)
+    .div(BigDecimal.fromString((dailySnapshot.snapshots.length + 1).toString()))
+
+  const snapshots = dailySnapshot.snapshots
+  snapshots.push(snapshot.id)
+  dailySnapshot.snapshots = snapshots
+
+  dailySnapshot.save()
+}
+
+function getDailyDEITokenPriceSnapshot(timestamp: BigInt): DailyDeiTokenPriceSnapshot {
+  const dailyId = (timestamp.toI32() / (24 * 60 * 60)).toString()
+  let dailySnapshot = DailyDeiTokenPriceSnapshot.load(dailyId)
+  if (!dailySnapshot) {
+    dailySnapshot = new DailyDeiTokenPriceSnapshot(dailyId)
+    dailySnapshot.timestamp = timestamp
+  }
+  return dailySnapshot
 }
